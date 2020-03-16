6c35ca270b2fa4d2f94653e4408fedde3a7593dd
John Lonergan <john.lonergan@gmail.com>
2020-03-16 02:18:40 +0000

    nothing

--- verilog/74245/hct74245.v
+++ verilog/74245/hct74245.v
@@ -13,14 +13,14 @@ module hct74245(
 );
 
     // HCT typical @ 5v according to https://assets.nexperia.com/documents/data-sheet/74HC_HCT245.pdf
-    // specify
-    // (A *> B) = (10);
-    // (B *> A) = (10);
-    // (dir *> A) = (16);
-    // (dir *> B) = (16);
-    // (nOE *> A) = (16);
-    // (nOE *> B) = (16);
-    // endspecify
+    specify
+        (A *> B) = (10);
+        (B *> A) = (10);
+        (dir *> A) = (16);
+        (dir *> B) = (16);
+        (nOE *> A) = (16);
+        (nOE *> B) = (16);
+    endspecify
 
     // always @(*) 
     //     $display("74245 dir=%1b", dir, " nOE=%1b", nOE, " A= %-8b ", A, " B =%-8b ", B);
