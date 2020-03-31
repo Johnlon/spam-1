5ad8e3aeb4eff5b6d5822870c23a3b9ba524c50e
John Lonergan <john.lonergan@gmail.com>
2020-03-31 01:06:41 +0100

    added 7474

---
+++ verilog/7474/hct7474.v
@@ -0,0 +1,91 @@
+// Dual D flip-flop with set and clear; positive-edge-triggered
+// Timings from https://assets.nexperia.com/documents/data-sheet/74HC_HCT74.pdf
+
+// Corrected - https://raw.githubusercontent.com/TimRudy/ice-chips-verilog/master/source-7400/7474.v
+// 
+// TimRudy version doesn't match datasheet
+// - the clear/preset should be async (see note below)
+// - clear/preset should be not be edge sensitive (see note below)
+// - when clear/preset are both set then Q and Qb are both 1
+
+// Tim Rudy's version has these compromises.
+//      Note: Note: Preset_bar is synchronous, not asynchronous as specified in datasheet for this device,
+//      in order to meet requirements for FPGA circuit design (see IceChips Technical Notes)
+
+module ttl_7474 #(parameter BLOCKS = 2, DELAY_RISE = 18, DELAY_FALL = 18)
+(
+  input [BLOCKS-1:0] Preset_bar,
+  input [BLOCKS-1:0] Clear_bar,
+  input [BLOCKS-1:0] D,
+  input [BLOCKS-1:0] Clk,
+  output [BLOCKS-1:0] Q,
+  output [BLOCKS-1:0] Q_bar
+);
+
+//------------------------------------------------//
+reg [BLOCKS-1:0] Q_current;
+reg [BLOCKS-1:0] Qb_current;
+reg [BLOCKS-1:0] Preset_bar_previous;
+reg [BLOCKS-1:0] Q_defined = 0;
+
+generate
+  genvar i;
+  for (i = 0; i < BLOCKS; i = i + 1)
+  begin: gen_blocks
+    always @(posedge Clk[i])
+    begin
+      if (Clear_bar[i] && Preset_bar[i])
+      begin
+        //$display("Setting");
+        Q_defined[i] <= 1'b1;
+        Q_current[i] <= D[i];
+        Qb_current[i] <= !D[i];
+      end
+        else
+        begin
+        //$display("Setting disabled by Clear or Preset");
+        end
+    end
+
+    always @(Clear_bar[i] or Preset_bar[i])
+    begin
+      if (!Clear_bar[i] && !Preset_bar[i])
+        begin
+            //$display("Clear and Preset");
+            Q_current[i] <= 1'b1;
+            Qb_current[i] <= 1'b1;
+        end
+      else if (!Clear_bar[i])
+        begin
+            //$display("Clear");
+            
+            Q_defined[i] <= 1'b1;
+            Q_current[i] <= 1'b0;
+            Qb_current[i] <= 1'b1;
+        end
+      else if (!Preset_bar[i])
+        begin
+            //$display("Preset");
+            Q_defined[i] <= 1'b1;
+            Q_current[i] <= 1'b1;
+            Qb_current[i] <= 1'b0;
+        end
+      else //
+        begin
+            //$display("Not Clear and Not Preset");
+            if (!Q_defined[i]) begin
+                // no value has been defined - realistically a random value would be settled on, we'll use X
+                Q_current[i] <= 1'bx;
+                Qb_current[i] <= 1'bx;
+            end
+        end
+      end
+  end
+endgenerate
+//------------------------------------------------//
+
+assign #(DELAY_RISE, DELAY_FALL) Q = Q_current;
+assign #(DELAY_RISE, DELAY_FALL) Q_bar = Qb_current;
+
+endmodule
+
---
+++ verilog/7474/test.v
@@ -0,0 +1,142 @@
+`include "hct7474.v"
+`include "../lib/assertion.v"
+
+module tb();
+
+logic cp, d, sd, rd;
+wire q, qb;
+
+localparam BLOCKS = 1;
+
+// names: https://assets.nexperia.com/documents/data-sheet/74HC_HCT74.pdf
+ttl_7474 #(.BLOCKS(BLOCKS), .DELAY_RISE(5), .DELAY_FALL(3)) dut(
+  .Preset_bar(sd),
+  .Clear_bar(rd),
+  .D(d),
+  .Clk(cp),
+  .Q(q),
+  .Q_bar(qb)
+);
+
+initial begin
+    $monitor("cp=%1b,d=%1b,sd=%1b, rd=%1b, q=%1b, qb=%1b", cp,d,sd, rd, q, qb);
+
+    cp=0;
+    d=1;
+
+    #10
+    rd=1;
+    sd=1;
+
+    #10
+    `Equals(q, 1'bx);
+    `Equals(qb, 1'bx);
+
+    #10
+    rd=0;
+    sd=0;
+
+    #10
+    `Equals(q, 1'b1);
+    `Equals(qb, 1'b1);
+
+    #10
+    rd=1;
+    sd=1;
+
+    #10
+    `Equals(q, 1'bx); // still no definite value set
+    `Equals(qb, 1'bx); // still no definite value set
+
+    // should reset async
+    #10
+    rd=0;
+    sd=1;
+
+    #10
+    `Equals(q, 1'b0);
+    `Equals(qb, 1'b1); 
+
+    // should stay same
+    #10
+    rd=1;
+    sd=1;
+
+    #10
+    `Equals(q, 1'b0);
+    `Equals(qb, 1'b1); 
+
+    // should set async
+    #10
+    rd=1;
+    sd=0;
+
+    #10
+    `Equals(q, 1'b1);
+    `Equals(qb, 1'b0); 
+
+    // should stay same
+    #10
+    rd=1;
+    sd=1;
+
+    #10
+    `Equals(q, 1'b1);
+    `Equals(qb, 1'b0); 
+
+    // should stay same even if d changes
+    #10
+    d=0;
+
+    #10
+    `Equals(q, 1'b1);
+    `Equals(qb, 1'b0); 
+
+    // should override clock +ve edge with set
+    #10
+    rd=1;
+    sd=0;
+    cp=1;
+
+    #10
+    `Equals(q, 1'b1);
+    `Equals(qb, 1'b0); 
+
+
+    // should clock in d
+    d=0;
+    rd=1;
+    sd=1;
+    cp=0;
+
+    #10
+    cp=1;
+
+    #10
+    `Equals(q, 1'b0);
+    `Equals(qb, 1'b1); 
+
+    // should clock in d
+    d=1;
+    rd=1;
+    sd=1;
+    cp=0;
+
+    #10
+    cp=1;
+
+    #10
+    `Equals(q, 1'b1);
+    `Equals(qb, 1'b0); 
+
+    // should clear async
+    d=1;
+    rd=0;
+    sd=1;
+
+    #10
+    `Equals(q, 1'b0);
+    `Equals(qb, 1'b1); 
+end
+
+endmodule
