cd83f36552c0763f06ccb4e9a71e97b77a1783a4
John Lonergan <john.lonergan@gmail.com>
2020-04-01 04:28:41 +0100

    more consistent naming

--- verilog/ram/ram.v
+++ verilog/ram/ram.v
@@ -4,18 +4,18 @@
 `timescale 1ns/100ps
 
 // verilator lint_off UNOPTFLAT
-module ram(_OE, _WE, addr, d);
+module ram(_OE, _WE, A, D);
 
 parameter DWIDTH=8,AWIDTH=16, DEPTH= 1 << AWIDTH;
 
 input _OE, _WE;
-input [AWIDTH-1:0] addr;
-inout [DWIDTH-1:0] d;
+input [AWIDTH-1:0] A;
+inout [DWIDTH-1:0] D;
  
 reg [DWIDTH-1:0] mem [DEPTH-1:0];
 logic [7:0] dout;
 
-assign d = dout;
+assign D = dout;
 
 // always @*
 //     $monitor("RAM : _OE=%1b, _WE=%1b, addr=%8b, d=%8b, m0=%8b, m1=%8b, m2=%8b",_OE, _WE, addr, d, mem[0], mem[1], mem[2]);
@@ -24,23 +24,25 @@ assign d = dout;
    begin
     // NOTE: 6116 and 62256 RAM datasheet says _WE overrides _OE but I want to detect this unexpected situation
      if (!_WE && !_OE) begin
-       $display("RAM cannot be _OE and _WE simultaneously");
-       $finish;
+       $display("ALERT - RAM cannot be _OE and _WE simultaneously - WRITE WINS - RAM[0x%04x]=0x%02x", A, D );
+       // $finish;
      end
    end
 
-  always @(_WE or d or addr)
+  always @(_WE or D or A)
    begin
      if (!_WE) begin
         dout = {DWIDTH{1'bz}};
-        mem[addr] = d;
+        mem[A] = D;
      end
    end
 
-  always @(_OE or addr)
+  always @(_OE or A)
    begin
       if (!_OE && _WE) 
-        dout = mem[addr];
+        dout = mem[A];
+
+        //if (dout == 8'bxxxxxxxx) dout=99; // MARKER VALUE FOR UNASSIGNED - TEMPORARY
    end
 
   integer i;
