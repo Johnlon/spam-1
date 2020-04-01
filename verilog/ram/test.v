d149a7611c35d5ff2e62eb8e9b962da9c7a56a9a
John Lonergan <john.lonergan@gmail.com>
2020-04-01 04:31:15 +0100

    naming and logging

--- verilog/ram/test.v
+++ verilog/ram/test.v
@@ -6,59 +6,59 @@
 module test();
 
  logic _OE, _WE;
- logic [7:0] addr;
- tri [7:0] d;
+ logic [7:0] A;
+ tri [7:0] D;
  
  logic [7:0] Vd;
- assign d=Vd;
+ assign D=Vd;
 
-ram #(.AWIDTH(8), .DEPTH(16)) r1(._OE, ._WE, .addr, .d);
+ram #(.AWIDTH(8), .DEPTH(16)) r1(._OE, ._WE, .A, .D);
 
  initial begin
-    assign addr=8'b00000001;
+    assign A=8'b00000001;
     Vd=8'b00000001;
     _OE=1'b1;
     _WE=1'b0;
     #100
     _WE=1'b1;
     #100
-    $display("TEST: _OE=%1b, _WE=%1b, addr=%8b, d=%8b - setup write 1 @ 1",_OE, _WE, addr, d);
+    $display("TEST: _OE=%1b, _WE=%1b, A=%8b, D=%8b - setup write 1 @ 1",_OE, _WE, A, D);
  
-    assign addr=8'b00000010;
+    assign A=8'b00000010;
     Vd=8'b00000010;
     _OE=1'b1;
     _WE=1'b0;
     #100
     _WE=1'b1;
     #100
-    $display("TEST: _OE=%1b, _WE=%1b, addr=%8b, d=%8b - setup write 2 @ 2",_OE, _WE, addr, d);
+    $display("TEST: _OE=%1b, _WE=%1b, A=%8b, D=%8b - setup write 2 @ 2",_OE, _WE, A, D);
  
     Vd=8'bzzzzzzzz;
     #100
-    $display("TEST: _OE=%1b, _WE=%1b, addr=%8b, d=%8b - nothing asserted",_OE, _WE, addr, d);
-    `Equals(d, 8'bzzzzzzzz);
+    $display("TEST: _OE=%1b, _WE=%1b, A=%8b, D=%8b - nothing asserted",_OE, _WE, A, D);
+    `Equals(D, 8'bzzzzzzzz);
 
-    assign addr=8'b00000010;
+    assign A=8'b00000010;
     _OE=1'b0;
     #100
-    $display("TEST: _OE=%1b, _WE=%1b, addr=%8b, d=%8b - read back 2",_OE, _WE, addr, d);
-    `Equals(d, 8'b00000010);
+    $display("TEST: _OE=%1b, _WE=%1b, A=%8b, D=%8b - read back 2",_OE, _WE, A, D);
+    `Equals(D, 8'b00000010);
  
-    assign addr=8'b00000001;
+    assign A=8'b00000001;
     #100
-    $display("TEST: _OE=%1b, _WE=%1b, addr=%8b, d=%8b - read back 1 ",_OE, _WE, addr, d);
-    `Equals(d, 8'b00000001);
+    $display("TEST: _OE=%1b, _WE=%1b, A=%8b, D=%8b - read back 1 ",_OE, _WE, A, D);
+    `Equals(D, 8'b00000001);
  
-    assign addr=8'b00000000;
+    assign A=8'b00000000;
     #100
-    $display("TEST: _OE=%1b, _WE=%1b, addr=%8b, d=%8b - read back 0 ",_OE, _WE, addr, d);
-    `Equals(d, 8'bxxxxxxxx);
+    $display("TEST: _OE=%1b, _WE=%1b, A=%8b, D=%8b - read back 0 ",_OE, _WE, A, D);
+    `Equals(D, 8'bxxxxxxxx);
  
     // this will trip an exception trap in the RAM - but I can't test that - uncomment 
     // _OE=1'b0;
     // _WE=1'b0;
     // #100
-    // $display("TEST: _OE=%1b, _WE=%1b, addr=%8b, d=%8b - cannot enable write and output at same time",_OE, _WE, addr, d);
+    // $display("TEST: _OE=%1b, _WE=%1b, A=%8b, d=%8b - cannot enable write and output at same time",_OE, _WE, addr, d);
     // `Equals(d, 8'bzzzzzzzz);
  
  end
--- verilog/rom/test.v
+++ verilog/rom/test.v
@@ -8,31 +8,31 @@
 module test();
 
  logic _OE, _CS;
- logic [3:0] Address;
- tri [7:0] Data;
+ logic [3:0] A;
+ tri [7:0] D;
  
-rom #(.AWIDTH(4), .DEPTH(16)) r1(._CS, ._OE, .Address, .Data);
+rom #(.AWIDTH(4), .DEPTH(16)) r1(._CS, ._OE, .A, .D);
 
  initial begin
     _OE=1'b1;
     _CS=1'b0;
     #100
-    $display("TEST: _OE=%1b, _CS=%1b, Address=%8b, d=%8b - output disabled",_OE, _CS, Address, Data);
-     `Equals(Data, 8'bzzzzzzzz);
+    $display("TEST: _OE=%1b, _CS=%1b, A=%8b, d=%8b - output disabled",_OE, _CS, A, D);
+     `Equals(D, 8'bzzzzzzzz);
 
-    assign Address=4'b0010;
+    assign A=4'b0010;
     _OE=1'b0;
     _CS=1'b0;
     #100
-    $display("TEST: _OE=%1b, _CS=%1b, Address=%8b, d=%8b - setup write 2 @ 2",_OE, _CS, Address, Data);
-     `Equals(Data, 8'b00000010);
+    $display("TEST: _OE=%1b, _CS=%1b, A=%8b, d=%8b - setup write 2 @ 2",_OE, _CS, A, D);
+     `Equals(D, 8'b00000010);
  
-    assign Address=4'b1111;
+    assign A=4'b1111;
     _OE=1'b0;
     _CS=1'b0;
     #100
-    $display("TEST: _OE=%1b, _CS=%1b, Address=%8b, d=%8b - setup write 2 @ 2",_OE, _CS, Address, Data);
-     `Equals(Data, 8'b00001111);
+    $display("TEST: _OE=%1b, _CS=%1b, A=%8b, d=%8b - setup write 2 @ 2",_OE, _CS, A, D);
+     `Equals(D, 8'b00001111);
  
  end
 
