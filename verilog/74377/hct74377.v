d294bae3285ad3ac1ffe2838751e9ef125b842b4
John Lonergan <john.lonergan@gmail.com>
2020-04-11 22:47:51 +0100

    noop

---
+++ verilog/74377/hct74377.v
@@ -0,0 +1,29 @@
+// based on https://raw.githubusercontent.com/TimRudy/ice-chips-verilog/master/source-7400/74377.v
+// timings from https://assets.nexperia.com/documents/data-sheet/74HC_HCT377.pdf
+
+// Octal D flip-flop with enable
+
+`timescale 1ns/1ns
+
+module hct74377 #(parameter WIDTH = 8, DELAY_RISE = 7, DELAY_FALL = 7)
+(
+  input _EN,
+  input CP,
+  input [WIDTH-1:0] D,
+  output [WIDTH-1:0] Q
+);
+
+//------------------------------------------------//
+reg [WIDTH-1:0] Q_current;
+
+always @(posedge CP)
+begin
+  if (!_EN)
+    Q_current <= D;
+end
+//------------------------------------------------//
+
+assign #(DELAY_RISE, DELAY_FALL) Q = Q_current;
+
+endmodule
+
