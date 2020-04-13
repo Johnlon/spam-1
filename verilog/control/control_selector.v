3ec69164a27e31a7372aa5db5d431a8d332d3483
John Lonergan <john.lonergan@gmail.com>
2020-04-13 02:56:44 +0100

    updated count program

--- verilog/control/control_selector.v
+++ verilog/control/control_selector.v
@@ -48,25 +48,34 @@ module control_selector #(parameter LOG=0)
     wire [7:0] _decodedOp;
     hct74138 opDecoder(.Enable3(1'b1), .Enable2_bar(1'b0), .Enable1_bar(1'b0), .A(operation_sel), .Y(_decodedOp));
     
-    // BUS ACCESS
-    assign  #18 _rom_out = _decodedOp[op_DEV_eq_ROM_sel];
-    assign  #28 _ram_out = _decodedOp[op_DEV_eq_RAM_sel] && _decodedOp[op_DEV_eq_RAMZP_sel];
-    assign  #28 _alu_out = _decodedOp[op_NONREG_eq_OPREGY_sel] && _decodedOp[op_REGX_eq_ALU_sel] && _decodedOp[op_RAMZP_eq_REG_sel];
-    assign  #28 _uart_out = _decodedOp[op_DEV_eq_UART_sel] && _decodedOp[op_RAMZP_eq_UART_sel];
+    // BUS ACCESS - FIXME - short sikes of high current possible.
+    // With logic gates this setup causes contention due to differences in timings on the _out's. 
+    // A rom based approach might also be glitchy due to unpredicable values during transitions.
+    // Diode logic would minimise timings diffs - very short periods of randomness during transitions.
+    // Correct approach is not to use 74245's etc and instead to connect to the bus using open collector outputs.
+    // 3 input AND = 11ns - https://assets.nexperia.com/documents/data-sheet/74HC_HCT11.pdf
+    // 2 input AND = 11ns - https://assets.nexperia.com/documents/data-sheet/74HC_HCT08.pdf
+    // inverter = 8ns - https://assets.nexperia.com/documents/data-sheet/74HC_HCT04.pdf
+    assign  _rom_out = _decodedOp[op_DEV_eq_ROM_sel];
+    assign  #11 _ram_out = _decodedOp[op_DEV_eq_RAM_sel] && _decodedOp[op_DEV_eq_RAMZP_sel];
+    assign  #11 _uart_out = _decodedOp[op_DEV_eq_UART_sel] && _decodedOp[op_RAMZP_eq_UART_sel];
+    assign  #11 _alu_out = _decodedOp[op_NONREG_eq_OPREGY_sel] && _decodedOp[op_REGX_eq_ALU_sel] && _decodedOp[op_RAMZP_eq_REG_sel];
 
     // _ram_zp will turn off the ram address buffers letting HiAddr pull down to 0 and will turn on ROM->MARLO for the lo addr
-    assign #28 _ram_zp = _decodedOp[op_DEV_eq_RAMZP_sel] && _decodedOp[op_RAMZP_eq_REG_sel] && _decodedOp[op_RAMZP_eq_UART_sel];
+    assign #11 _ram_zp = _decodedOp[op_DEV_eq_RAMZP_sel] && _decodedOp[op_RAMZP_eq_REG_sel] && _decodedOp[op_RAMZP_eq_UART_sel];
     
-    assign #28 force_x_val_to_zero = !_decodedOp[op_NONREG_eq_OPREGY_sel];  // +ve logic needed - EXTRA GATE
-    assign #28 force_alu_op_to_passx = !_decodedOp[op_RAMZP_eq_REG_sel]; // +ve logic needed - EXTRA GATE
+    assign #8 force_x_val_to_zero = !_decodedOp[op_NONREG_eq_OPREGY_sel];  // +ve logic needed - EXTRA GATE
+    assign #8 force_alu_op_to_passx = !_decodedOp[op_RAMZP_eq_REG_sel]; // +ve logic needed - EXTRA GATE
     
     // write device - sometimes bit 0 is a device bit and sometimes ALU op bit
-    wire _ram_write_override = _decodedOp[op_RAMZP_eq_REG_sel] && _decodedOp[op_RAMZP_eq_UART_sel];
-    wire _is_non_reg_override = _decodedOp[op_NONREG_eq_OPREGY_sel] && _ram_write_override;
-    wire _is_reg_override = _decodedOp[op_REGX_eq_ALU_sel];
+    wire #11 _ram_write_override = _decodedOp[op_RAMZP_eq_REG_sel] && _decodedOp[op_RAMZP_eq_UART_sel];
+    wire #11 _is_non_reg_override = _decodedOp[op_NONREG_eq_OPREGY_sel] && _ram_write_override;
+    wire #11 _is_reg_override = _decodedOp[op_REGX_eq_ALU_sel];
     wire implied_dev_top_bit = hi_rom[0];
 
-    wire reg_in = (implied_dev_top_bit && _is_non_reg_override) || !_is_reg_override; // EXTRA GATE
+    wire #11 implied_reg_in_not_overridden = implied_dev_top_bit && _is_non_reg_override;
+    wire #8 is_reg_override = ! _is_reg_override; // EXTRA GATE
+    wire #11 reg_in = implied_reg_in_not_overridden || is_reg_override; // EXTRA GATE
 
     wire [4:0] device_sel_pre = {reg_in, hi_rom[4:1]}; // pull down top bit if this instruction applies to non-reg as that bit is used by ALU
 
@@ -80,8 +89,8 @@ module control_selector #(parameter LOG=0)
     // return
     assign #18 device_in = device_sel_out[4:0];
 
-if (1)    always @ * 
-         $display("%6d CRTLSEL", $time,
+if (LOG)    always @ * 
+         $display("%8d CTRL_SEL", $time,
           " hi=%08b", hi_rom, 
             " _rom_out=%1b, _ram_out=%1b, _alu_out=%1b, _uart_out=%1b", _rom_out, _ram_out, _alu_out, _uart_out,
             " device_in=%5b", device_in, 
