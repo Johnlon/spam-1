76eaf3ef6310e7595a8dc431e1e5492f2e829d39
John Lonergan <john.lonergan@gmail.com>
2020-04-13 02:58:34 +0100

    added log control

--- verilog/ram/ram.v
+++ verilog/ram/ram.v
@@ -7,6 +7,8 @@
 module ram(_OE, _WE, A, D);
 
 parameter DWIDTH=8,AWIDTH=16, DEPTH= 1 << AWIDTH;
+parameter LOG=0;
+
 
 input _OE, _WE;
 input [AWIDTH-1:0] A;
@@ -17,10 +19,11 @@ logic [7:0] dout;
 
 assign D = _OE? {DWIDTH{1'bz}}: dout;
 
-// always @*
-//     $monitor("RAM : _OE=%1b, _WE=%1b, addr=%8b, d=%8b, m0=%8b, m1=%8b, m2=%8b",_OE, _WE, addr, d, mem[0], mem[1], mem[2]);
+  if (LOG)
+    always @(_WE or _OE or A or D)
+     $monitor("%8d RAM : _OE=%1b _WE=%1b A=%04x D=%8b m0=%8b m1=%8b m2=%8b", $time, _OE, _WE, A, D, mem[0], mem[1], mem[2]);
 
-  always @(_WE or _OE)
+  if (LOG) always @(_WE or _OE or A or D)
    begin
     // NOTE: 6116 and 62256 RAM datasheet says _WE overrides _OE but I want to detect this unexpected situation
      if (!_WE && !_OE) begin
