9ab797013bf86eb2656a0ce8805b1b0ef76e6889
John Lonergan <john.lonergan@gmail.com>
2020-04-20 11:47:13 +0100

    uart remote working

--- verilog/uart/test.v
+++ verilog/uart/test.v
@@ -1,95 +1,111 @@
+/* verilator lint_off ASSIGNDLY */
+/* verilator lint_off STMTDLY */
+
 `include "um245r.v"
 `include "../lib/assertion.v"
 `timescale 1ns/1ns
 
 module test();
 
-// 33 exclamation mark
-tri [7:0] D;
-logic [7:0] Dv;
-logic WR;
-logic _RD;
-wire _TXE;
-wire _RXF;
+    integer verbose = 0;
 
-um245r #(.FILENAME("data.mem"), .DEPTH(12))  uart (
-    .D,	                // Input data
-    .WR,		// Writes data on -ve edge
-    ._RD,		// When goes from high to low then the FIFO data is placed onto D (equates to _OE)
-    ._TXE,		// When high do NOT write data using WR, when low write data by strobing WR
-    ._RXF		// When high to NOT read from D, when low then data is available to read by strobing RD low
-  );
+    // 33 exclamation mark
+    tri [7:0] D;
+    logic [7:0] Dtx;
+    logic WR;
+    logic _RD;
+    wire _TXE;
+    wire _RXF;
+    reg RECEIVED;
 
-assign D=Dv;
+    localparam T9 = 20;
+    localparam T10 = 0;
 
-reg [12*8:0] expected;
+    assign D=Dtx;
 
-initial begin
+    reg [12*8:0] expected;
+    integer countTx=0;
+    integer countRx=0;
+    integer MAX_RX=10;
 
-    _RD=1;
+    logic clk;
 
-`define CLK_WR \
-    #100 \
-    WR=0; \
-    #100 \
-    WR=1;
+    initial begin
 
-    $display("WRITING ....");
-    //'!'=33;
-    Dv=33;
-    `CLK_WR
-    `CLK_WR
-    `CLK_WR
-    `CLK_WR
-    `CLK_WR
+        Dtx=8'bzzzzzzzz;
+        $display("%t TEST: START", $time);
 
-    // READING ....
-    $display("\nREADING ....");
-    #100
-    WR=1;
-    Dv=8'bz;
-    
-`define CLK_RD(EXPECTED) \
-    #100 \
-    `Equals(_RXF, 1'b0); \
-    _RD=0; \
-    #100 \
-    if ( D !== 8'bxxxxxxxx ) \
-        $write("%c", D); \
-    else \
-        $write("%c(%2x) %c(%2x)", D, D, EXPECTED, EXPECTED); \
-    `Equals(D, EXPECTED); \
-    #100 \
-    _RD=1;
+        _RD=1;
+        WR=1; 
 
-    // data available?
-    #100 
-    `Equals(_RXF, 1'b0); 
-    #100 
-    
-    // read data
-    expected = "Hello World!";
-    `CLK_RD(expected[12*8-1:(12*8)-8])
-    `CLK_RD(expected[11*8-1:(11*8)-8])
-    `CLK_RD(expected[10*8-1:(10*8)-8])
-    `CLK_RD(expected[09*8-1:(09*8)-8])
-    `CLK_RD(expected[08*8-1:(08*8)-8])
-    `CLK_RD(expected[07*8-1:(07*8)-8])
-    `CLK_RD(expected[06*8-1:(06*8)-8])
-    `CLK_RD(expected[05*8-1:(05*8)-8])
-    `CLK_RD(expected[04*8-1:(04*8)-8])
-    `CLK_RD(expected[03*8-1:(03*8)-8])
-    `CLK_RD(expected[02*8-1:(02*8)-8])
-    `CLK_RD(expected[01*8-1:(01*8)-8])
+        clk=0;
+        forever begin
+            #100  clk =  ! clk; 
+        end
 
-    $display("\nREAD ALL");
+        #5000 
+        $display("%t TEST: END", $time);
 
-    // no data left
-    #100 
-    `Equals(_RXF, 1'b1); 
+    end
 
-    $display("\nEND");
-end
+    //um245r #(.OUTPUT_FILE("out.txt"), .INPUT_FILE("data.mem"), .INPUT_FILE_DEPTH(12))  uart (
+    um245r #(.LOG(1), .HEXMODE(1), .OUTPUT_FILE("/dev/stdout"), .INPUT_FILE("/dev/stdin"), .INPUT_FILE_DEPTH(12))  uart (
+        .D,	                // Input data
+        .WR,		// Writes data on -ve edge
+        ._RD,		// When goes from high to low then the FIFO data is placed onto D (equates to _OE)
+        ._TXE,		// When high do NOT write data using WR, when low write data by strobing WR
+        ._RXF		// When high to NOT read from D, when low then data is available to read by strobing RD low
+      );
+
+    always @(negedge _RXF) begin
+        countRx ++;
+        if (verbose) $display("================================= READ ==================", countRx);
+        #20 
+        if (verbose) $display("%t TEST: Start Read : _RD goes low", $time);
+        _RD=0;
+
+        #151 // T3 delay
+        RECEIVED=D;
+        $display("%t TEST: ", $time, "Bin=%8b Hex=%02x Char=%c", D, D, D, " RECEIVED");
+
+        #10
+        _RD=1;
+        if (verbose) $display("%t TEST: Finished Read : _RD goes high", $time);
+
+        if (countRx == MAX_RX) begin
+            $display("%t TEST: ABORT", $time);
+            $finish();
+        end
+    end
+
+    always @(negedge _TXE) begin
+        countTx = countTx + 1;
+
+        // WR is -ve edge triggered - strobe it H then L
+        $display("TEST: Strobing WR=1 BEGIN write of countTx=%-d", countTx);
+        WR=1;
+
+        Dtx=countTx;
+        #T9
+        $display("TEST: Strobing WR=0 LATCH TRANSMIT");
+        WR=0;
+
+        #T10
+        $display("TEST: Strobing END BUS=z");
+        Dtx=8'bzzzzzzzz;
+
+    end
+
+    always @(*)
+        if (verbose)
+        $display("%t TEST: ", $time, 
+            "D=%8b", D, 
+            ", WR=%1b", WR, 
+            ",_RD=%1b", _RD, 
+            " _RXF=%1b", _RXF, 
+            " _TXE=%1b", _TXE, 
+            " Dtx=%8b", Dtx
+            );
 
 endmodule
 
--- verilog/uart/um245r.v
+++ verilog/uart/um245r.v
@@ -2,45 +2,236 @@
 /* verilator lint_off ASSIGNDLY */
 
 `timescale 1ns/1ns
-module um245r #(parameter DELAY=50, FILENAME="", DEPTH=0, HEXMODE=0, LOG=0)  (
-        inout [7:0] D,	// Input data
-	input WR,		// Writes data on -ve edge
-	input _RD,		// When goes from high to low then the FIFO data is placed onto D (equates to _OE)
-	output _TXE,		// When high do NOT write data using WR, when low write data by strobing WR
-	output _RXF		// When high to NOT read from D, when low then data is available to read by strobing RD low
-  );
+`define EOF 32'hFFFF_FFFF 
+`define NULL 0 
+`define EOF 32'hFFFF_FFFF
 
-  reg [7:0] Mem [0:DEPTH-1];
+module um245r #(parameter T3=50, T4=1, T5=25, T6=80, T11=25, T12=80,
+        INPUT_FILE="", OUTPUT_FILE="", INPUT_FILE_DEPTH=0, HEXMODE=0, LOG=0)  (
+            inout [7:0] D,    // Input data
+    input WR,        // Writes data on -ve edge
+    input _RD,        // When goes from high to low then the FIFO data is placed onto D (equates to _OE)
+ 
+    output _TXE,        // When high do NOT write data using WR, when low write data by strobing WR
+    output _RXF        // When high to NOT read from D, when low then data is available to read by strobing RD low
+);
 
-  // Initialise ROM from file lazily
-  initial begin
-    if (DEPTH > 0)
-      $readmemh(FILENAME, Mem);
-  end
+localparam MAX_LINE_LENGTH=80;
 
-  // UART output
-  assign _TXE=0; // always ready to transmit
+integer fOut=`NULL, fControl, c, r, txLength, tDelta;
 
-  always @(negedge WR) begin
-    if (HEXMODE) $write("UART: %02x \n", D);
-    else $write("%c", D);
-  end
+reg [8*MAX_LINE_LENGTH:0] line; /* Line of text read from file */ 
+reg TX_READY;
+reg RX_READY;
 
-  integer i=-1;
+integer verbose=0;
+string str = "";
 
-  
-  // posedge so advance after reading
-  always @(posedge _RD) begin
-    i = i + 1;
-  end
+localparam BUFFER_SIZE=80;
 
-  /// small delay necessary to prevent previous value of Mem[i] appearing on wire 
-  assign #DELAY _RXF=!(i<DEPTH);
-  assign #DELAY D = _RD? 8'bzzzzzzzz: _RXF? 8'bxxxxxxxx: Mem[i];
+//reg [8*BUFFER_SIZE:0] rxBuf; // Line of text read from file 
+int rxBuf[BUFFER_SIZE]; // Line of text read from file 
+int absWritePos = 0; // next place to write
+int absReadPos = 0; // next place to read
 
-  if (LOG) always @*
-    $display("D=", D, ", WR=", WR, ",_RD=", _RD, ", i=", i, " MEM[%2d]=h%2x", i, Mem[i], " _RXF=%1b", _RXF);
 
+wire dataAvailable = absReadPos < absWritePos;
+wire writeAvailable = (absWritePos - absReadPos) < BUFFER_SIZE;
+
+reg [7:0] Drx = 'x;
+
+always @* begin
+    if (verbose)
+    $display("%t UART:", $time, 
+        " D=%8b", D, ", WR=%1b", WR, ",_RD=%1b", _RD, 
+        " _RXF=%1b", _RXF, 
+        " _TXE=%1b", _TXE, 
+        " Drx=%8b", Drx,
+        " ARPOS=%-3d", absReadPos,
+        " AWPOS=%-3d", absWritePos,
+        " RPOS=%-3d", absReadPos % BUFFER_SIZE,
+        " WPOS=%-3d", absWritePos % BUFFER_SIZE,
+        " DAVAIL=%1b", dataAvailable,
+        " TX_READY=%1b", TX_READY, 
+        " RX_READY=%1b", RX_READY
+        );
+
+end
+
+integer tx_count=0;
+assign _TXE = !(fOut != `NULL && TX_READY && tx_count > 0);
+assign _RXF = !(dataAvailable && RX_READY);
+
+assign #T3 D= _RD? 8'bzzzzzzzz: dataAvailable ? Drx : 8'bxzxzxzxz;
+
+/*
+    Transmit only valid when _TXE is low.
+    Transmit occurs when WR goes low.
+*/
+always @(negedge WR) begin
+    if (_TXE) begin
+            $display("%t ", $time, "UART: TRANSMITTING %8b", D);
+            $display("%t ", $time, "UART: WR low while _TXE not ready");
+            $finish_and_return(1);
+    end
+
+    $write("%t ", $time, "UART: TRANSMITTING 0x%02x (%c)\n", D, D);
+    $fwrite(fOut, "%02x\n", D);
+
+    #T11 // -WR to _TXE inactive delay
+    $display("TX NOT READY");
+    TX_READY=0; 
+
+    tx_count --;
+    if (tx_count < 0) begin
+            $display("%t ", $time, "UART: tx_count went negative");
+            $finish_and_return(1);
+    end
+
+    #T12 // min inactity period
+    if (verbose) $display("TX INACTIVE PERIOD ENDS");
+    TX_READY=1;
+
+end
+
+/*
+    Transmit only valid when _TXE is low.
+    Transmit occurs when WR goes low.
+*/
+always @(negedge _RD) begin
+    if (_RXF) begin
+            $display("%t ", $time, "UART: _RD low while _RXF not ready");
+            $finish_and_return(1);
+    end
+
+    if (! dataAvailable) begin
+            $display("%t ", $time, "UART: _RD low while data not available");
+            $finish_and_return(1);
+    end
+
+    //#T3 
+    //$display("0 = %d", rxBuf[0]);
+    //$display("1 = %d", rxBuf[1]);
+    //$display("2 = %d", rxBuf[2]);
+
+    if (verbose) $display("%t ", $time, "UART: READING AT %-d", absReadPos);
+    Drx = rxBuf[absReadPos%BUFFER_SIZE];
+    $display("%t ", $time, "UART: Received %02x (%c) from buf at %-d", Drx, Drx, absReadPos);
+    absReadPos++;
+end
+
+always @(posedge _RD) begin
+
+    #T11 // -WR to _TXE inactive delay
+    if (verbose) $display("%t ", $time, "UART: RX NOT READY");
+    RX_READY=0; 
+
+    #T12 // min inactity period
+    if (verbose) $display("%t ", $time, "UART: RX INACTIVE PERIOD ENDS");
+    RX_READY=1;
+end
+
+
+
+initial 
+    begin : file_block 
+    $timeformat(-9, 0, "ns", 6); 
+
+    for(int i=0; i<BUFFER_SIZE; i++) begin
+        rxBuf[i] = i;
+    end
+
+    RX_READY=0;
+    TX_READY=0;
+    #20
+
+    // FIXME
+    //absWritePos = 15; // next place to write into receive buffer
+
+    TX_READY=1;
+    #20
+
+    if (1) begin
+        $display("[%6t] ", $time, "opening fifo.control");
+        fControl = $fopenr("/tmp/fifo.control"); 
+        if (fControl == `NULL) // If error opening file 
+                disable file_block; // Just quit 
+
+        $display("[%6t] ", $time, "opening fifo.out");
+        fOut = $fopenw("/tmp/fifo.out"); 
+        if (fOut == `NULL) // If error opening file 
+                disable file_block; // Just quit 
+
+        $display("[%6t] ", $time, "fifos open");
+
+        while (fControl != `NULL)  
+        begin
+            c = $fgetc(fControl); 
+            while (c != `EOF) 
+            begin 
+                    /* Check the first character for comment */ 
+                    if (c == "/") // just skip
+                    begin 
+                        line="";
+                        r = $fscanf(fControl,"%s\n", line); 
+                        str = line; // string type removes trailing empty space
+                        $display("[%6t] ", $time, "/%s", str);
+                    end
+
+                    if (c == "r") // pass string back to simulatiom
+                    begin
+                        line="";
+                        r = $fscanf(fControl,"%s\n", line); 
+                        str = line; // string type removes trailing empty space
+
+                        $display("[%6t] ", $time, "RX: '%s'", str);
+
+                        for (int p=0; p<str.len() && writeAvailable; p++) begin
+                            rxBuf[absWritePos%BUFFER_SIZE] = str[p];
+                            absWritePos++;
+                        end
+                        if (! writeAvailable)
+                            $display("%t ", $time, "UART: RECEIVE BUFFER FULL");
+
+                        if (verbose) $display("%t ", $time, "UART: RECEIVE %3d / %3d", absWritePos, absWritePos%BUFFER_SIZE);
+                    end
+                    
+                    if (c == "t") // wait for simulation to transmit N chars
+                    begin
+                        txLength=0;
+                        r = $fscanf(fControl,"%d\n", txLength); 
+                        if (verbose) $display("[%6t] ", $time, "TX: waiting for %1d chars", txLength);
+                        tx_count = txLength;
+                    end
+                    
+                    if (c == "#") // skil N ns
+                    begin
+                        tDelta=0;
+                        r = $fscanf(fControl,"%d\n", tDelta); 
+                        $display("[%6t] ", $time, "#%1d begin", tDelta);
+                        #tDelta $display("[%6t] ", $time, "#%1d end", tDelta);
+                    end
+
+                    if (c == "q") // quit
+                    begin
+                        disable file_block; // Just quit 
+                    end
+
+                    c = $fgetc(fControl); // -1 == eof
+            end // while not EOF 
+        
+            $fclose(fControl); 
+                        
+            fControl = $fopenr("/tmp/fifo.control"); 
+            if (fControl == `NULL) // If error opening file 
+                    disable file_block; // Just quit 
+
+        end // restart read
+    end
+end // initial
 
 endmodule
+
+
+
 
