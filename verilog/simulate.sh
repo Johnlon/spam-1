ecaf0b25457fb0c8232d824d25af0a33c1234ab1
John Lonergan <john.lonergan@gmail.com>
2020-04-26 03:36:43 +0100

    logging

--- verilog/simulate.sh
+++ verilog/simulate.sh
@@ -6,14 +6,14 @@ then
 fi 
 
 cd $(dirname $(readlink -f $T.v))
-iverilog -Ttyp -Wall -g2012 -gspecify -o $T.vvp  $T.v 
+iverilog -Ttyp -Wall -g2012 -gspecify -grelative-include -o $T.vvp  $T.v 
 if [ $? != 0 ] ; then
-    echo ERROR 
+    echo ERROR exit code iverilog
     exit 1
 fi
 vvp -N -i $T.vvp
 if [ $? != 0 ] ; then
-    echo ERROR 
+    echo ERROR exit code vvp
     exit 1
 fi
 
