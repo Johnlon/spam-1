6ad5f439cb650e0d21c253205aac4af3a2c7d8b0
John Lonergan <john.lonergan@gmail.com>
2020-04-27 23:30:02 +0100

    added 74423 monostable

---
+++ verilog/74423/hct74423.v
@@ -0,0 +1,60 @@
+/*
+
+partial impl - for a single configuration where we get a negative pulse triggered by a positive edge
+
+74HCT423 RC MONOSTABLE
+    to https://www.mouser.co.uk/ProductDetail/Texas-Instruments/CD74HCT423E?qs=sGAEpiMZZMvlv4093HnhKZ8bAgIsdcd%2FsXgnN672tig%3D
+see also 74HCT123 RC MONOSTABLE
+    https://www.mouser.co.uk/ProductDetail/Texas-Instruments/CD74HCT123E?qs=sGAEpiMZZMvlv4093HnhKZ8bAgIsdcd%2FlNYxPTBpeBQ%3D
+ */
+`timescale 1ns/1ns
+module hct74423 (
+    input _A, B, _R, 
+    output Q, _Q
+);
+    
+    parameter PulseWidth=100;
+    parameter TriggerPD=25;
+
+    // NEEDED FOR SHORT /WE PULSE ON RISING EDGE OF CLOCK FOR
+    // 74HCT670 WHOSE MIN write pulse width is 20ns.
+    // HOW IN PRACTICE? RC? OR MORE GATES AS SHOWN HERE?
+
+    // simulate 74HCT123
+    reg q;
+    assign Q = q;
+    assign _Q = !Q;
+
+    always @* begin
+        if (_R) begin
+            if (_A) 
+                q=0;
+            else if (!B) 
+                q=0;
+        end
+        else
+        begin
+            q=0;
+        end
+    end
+    
+
+    always @(posedge B) begin
+        if (!_A) begin
+            #TriggerPD
+            q=1;
+            #PulseWidth
+            q=0;
+        end
+    end
+
+    always @(negedge _A) begin
+        if (B) begin
+            #TriggerPD
+            q=1;
+            #PulseWidth
+            q=0;
+        end
+    end
+    
+endmodule
---
+++ verilog/74423/test.v
@@ -0,0 +1,65 @@
+
+`include "hct74423.v"
+`include "../lib/assertion.v"
+
+`timescale 1ns/1ns
+
+module icarus_tb();
+    
+	logic _A,  B, _R;
+    wire Q, _Q;
+    
+	hct74423 #(.PulseWidth(100)) monostable(_A, B, _R, Q, _Q);
+    
+    initial begin
+        $dumpfile("dumpfile.vcd");
+        $dumpvars(0,  _A, B, _R, Q, _Q);
+    end
+
+    always @* $display ($time, "_A=%1b B=%1b _R=%1b  ->  Q=%1b _Q=%1b", _A, B, _R, Q, _Q);
+
+    initial begin
+        `Equals(Q, 1'bx);
+        `Equals(_Q, 1'bx);
+
+        #100
+        `Equals(Q, 1'bx);
+        `Equals(_Q, 1'bx);
+
+        $display("reset");
+        _R=0;
+        #100
+        `Equals(Q, 1'b0);
+        `Equals(_Q, 1'b1);
+            
+        $display("unreset");
+        _R=1;
+        #100
+        `Equals(Q, 1'b0);
+        `Equals(_Q, 1'b1);
+            
+        $display("prep");
+        _A=0;
+        B=0;
+        _R=1;
+        #100
+        `Equals(Q, 1'b0);
+        `Equals(_Q, 1'b1);
+
+        $display("trigger");
+        B=1;
+        #20 // less than PD
+        `Equals(Q, 1'b0);
+        `Equals(_Q, 1'b1);
+        #20 // not more than PD
+        `Equals(Q, 1'b1);
+        `Equals(_Q, 1'b0);
+
+        $display("pulse should have ended");
+        #100 // not more than PD
+        `Equals(Q, 1'b0);
+        `Equals(_Q, 1'b1);
+
+    end
+
+endmodule
