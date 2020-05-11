53f3d9b2e6aaa8b14870ad1b63b9f14c85c002ac
John Lonergan <john.lonergan@gmail.com>
2020-05-11 02:10:40 +0100

    added _sd and _rd

--- verilog/74109/hct74109.v
+++ verilog/74109/hct74109.v
@@ -6,38 +6,78 @@ module hct74109 (
     input j,
     input _k,
     input clk,
+    input _sd, _rd,
     output q, 
     output _q
 );
 
     parameter PROP_DELAY=17; 
+    parameter ASYNC_DELAY=15; 
     parameter SETUP_TIME=8;  // JK must be setup AHEAD fo the clock - see datasheet
 
     reg Q,J,_K;
+
+    logic fSR=1'bz;// force set / reset
+
+    wire Qresult = (fSR !== 1'bz)? fSR: Q;
  
     assign #(SETUP_TIME) J = j;
     assign #(SETUP_TIME) _K = _k;
 
-/*
-    specify
-        (clk => Q) = (PROP_DELAY);
-    endspecify
-*/
+    always @*
+        $display($time, " 74109  clk ", clk, 
+                    " j ", J, " _k ", _K, " q ", Qresult,
+                    " _sd " , _sd, " _rd ", _rd,
+                    " fSR ", fSR, " regQ ", Q
+                );
+
+    task checkSR;
+        // explicitely state args so these become part fo sensitivity list
+        // https://www.verilogpro.com/systemverilog-always_comb-always_ff/
+        input _sd, _rd;
+        output fSR;
+
+        if (!_sd & _rd) fSR<=1;
+        else if (_sd & !_rd) fSR<=0;
+        else if (!_sd & !_rd) fSR<=1;
+        else fSR<=1'bz;
+    endtask
+
+    initial begin
+        // explicitely state args so these become part fo sensitivity list
+        // https://www.verilogpro.com/systemverilog-always_comb-always_ff/
+        checkSR(_sd, _rd, fSR);
+    end 
+
+    always @* begin
+        // explicitely state args so these become part fo sensitivity list
+        // https://www.verilogpro.com/systemverilog-always_comb-always_ff/
+        checkSR(_sd, _rd, fSR);
+    end 
  
     always @(posedge clk)
     begin
-        case ({J,_K})
-            2'b01 :  Q <= Q;
-            2'b00 :  Q <= 0;
-            2'b11 :  Q <= 1;
-            2'b10 :  Q <= ~Q;
-        endcase
+        if (_sd & _rd) begin
+            case ({J,_K})
+                2'b01 :  
+                    #(PROP_DELAY) Q = Q;
+                2'b00 :  
+                    #(PROP_DELAY) Q = 0;
+                2'b11 :  
+                    #(PROP_DELAY) Q = 1;
+                2'b10 :  
+                    #(PROP_DELAY) Q = !Q;
+            endcase
+        end
 
-        //$display(" j ", J, " _k ", _K, " q ", Q);
     end
 
-    assign #(PROP_DELAY) q = Q;
-    assign #(PROP_DELAY) _q = !Q;
+    //assign #(PROP_DELAY) q = Q;
+    //assign #(PROP_DELAY) _q = !Q;
+
+
+    assign q = Qresult;
+    assign _q = !Qresult;
 
 endmodule
 
--- verilog/74109/test.v
+++ verilog/74109/test.v
@@ -7,6 +7,10 @@ module tb_jk;
    reg _k;
    reg clk=0;
 
+   reg _sd=1, _rd=1;
+
+   wire qS, _qS;
+   wire qR, _qR;
    wire q, _q;
  
    // expected PD of device
@@ -18,25 +22,57 @@ module tb_jk;
    parameter TEST_INTERVAL=CLK_T * 10;
 
    //always  #CLK_T clk = !clk;
-    task clk_pulse;
-    begin
+   task clk_pulse;
+   begin
         clk = 0;
         #CLK_T 
         clk = 1;
         #CLK_T 
         clk = 1; // repeat setting=1 otherwise syntax error cos can't have a delay as last item
-    end
-    endtask
+   end
+   endtask
 
  
+   hct74109  jkS ( .j(j),
+                  ._k(_k),
+                  .clk(clk),
+                  ._sd(1'b0),
+                  ._rd(1'b1),
+                  .q(qS),
+                  ._q(_qS)
+    );
+
+   hct74109  jkR ( .j(j),
+                  ._k(_k),
+                  .clk(clk),
+                  ._sd(1'b1),
+                  ._rd(1'b0),
+                  .q(qR),
+                  ._q(_qR)
+    );
+
    hct74109  jk0 ( .j(j),
                   ._k(_k),
                   .clk(clk),
+                  ._sd(_sd),
+                  ._rd(_rd),
                   .q(q),
                   ._q(_q)
     );
  
    initial begin
+            // hold
+      #TEST_INTERVAL 
+        $display("testing initial state set");
+        clk_pulse();
+        `Equals(qS, 1'b1)
+
+            // hold
+      #TEST_INTERVAL 
+        $display("testing initial state reset");
+        clk_pulse();
+        `Equals(qR, 1'b0)
+
             // hold
       #TEST_INTERVAL 
         $display("hold mode");
@@ -60,8 +96,12 @@ module tb_jk;
         `Equals(q, 1'b0)
         clk_pulse();
         `Equals(q, 1'b0)
-        clk_pulse();
-        `Equals(q, 1'b0)
+
+        $display("reset mode - async set");
+        _sd<=0;
+        #PD // NOT CLOCK
+        `Equals(q, 1'b1)
+        _sd<=1;
 
         // load set Q=H
       #TEST_INTERVAL 
@@ -76,6 +116,13 @@ module tb_jk;
         clk_pulse();
         `Equals(q, 1'b1)
 
+        $display("load mode - async set");
+        _rd<=0;
+        #PD // NOT CLOCK
+        `Equals(q, 1'b0)
+        _rd<=1;
+
+
         // toggle
       #TEST_INTERVAL 
         $display("toggle mode");
@@ -87,16 +134,12 @@ module tb_jk;
         `Equals(q, 1'b0)
         clk_pulse();
         `Equals(q, 1'b1)
-        clk_pulse();
-        `Equals(q, 1'b0)
-        clk_pulse();
-        `Equals(q, 1'b1)
 
       #TEST_INTERVAL $finish;
    end
  
    initial
-      $monitor ($time, " clk ", clk, " j=%0d _k=%0d q=%0d", j, _k, q);
+      $monitor ($time, " TEST   clk ", clk, " j %0d _k %0d q %0d", j, _k, q);
 
 endmodule  
  
