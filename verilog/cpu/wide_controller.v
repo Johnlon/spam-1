f4337d5cbf2cffda787bebfa1f8c54903e6706ba
John Lonergan <john.lonergan@gmail.com>
2020-07-15 19:22:40 +0100

    split out the assembler.v

--- verilog/cpu/wide_controller.v
+++ verilog/cpu/wide_controller.v
@@ -11,74 +11,73 @@
 `include "../alu/alu.v"
 `include "../control/control.v"
 
+`include "psuedo_assembler.v"
+
+`timescale 1ns/1ns
+
 // verilator lint_off ASSIGNDLY
 // verilator lint_off STMTDLY
 // verilator lint_off MULTITOP
 // verilator lint_off DECLFILENAME
 // verilator lint_off UNUSED
-
-`define OUT_ADEV_SEL(DNAME) output _adev_``DNAME``
-`define OUT_BDEV_SEL(DNAME) output _bdev_``DNAME``
-`define OUT_TDEV_SEL(DNAME) output _``DNAME``_in
-
-`timescale 1ns/1ns
-`define DIRECT 1'b1
-`define REGISTER 1'b0
-`define NA_AMODE 1'b0
-
-// PSEUDO ASSEMBLER
-`define ROM(A) { CPU.ctrl.rom_6.Mem[A], CPU.ctrl.rom_5.Mem[A], CPU.ctrl.rom_4.Mem[A], CPU.ctrl.rom_3.Mem[A], CPU.ctrl.rom_2.Mem[A], CPU.ctrl.rom_1.Mem[A] }
-
-// Instruction populates the ROM and adds a text version of the instruction to the CODE array
-`define INSTRUCTION(LOCN, TARGET, SRCA, SRCB, ALUOP, AMODE, ADDRESS, IMMED) \
-    `ROM(LOCN) = { `toALUOP(ALUOP), cast.to5(`toDEV(TARGET)), cast.to4(`toDEV(SRCA)), cast.to4(`toDEV(SRCB)), 5'bz, AMODE, cast.to16(ADDRESS), cast.to8(IMMED) }; \
-    CODE[LOCN] = "TARGET=SRCA(ALUOP)SRCB  amode=AMODE immed8=IMMED addr=ADDRESS";
-
-`define NA 'z
-
-// WARNING : instreg in the X position is High Impedance as its only attached to the R bus
-`define DEV_EQ_XI_ALU(INST, TARGET, SRCA, IMMED8, ALUOP) `INSTRUCTION(INST, TARGET, SRCA,    instreg,    ALUOP, `REGISTER, `NA,     IMMED8)
-
-`define DEV_EQ_XY_ALU(INST, TARGET, SRCA, SRCB, ALUOP) `INSTRUCTION(INST, TARGET, SRCA,     SRCB,    ALUOP, `REGISTER, `NA,     `NA)
-`define DEV_EQ_ROM_DIRECT(INST,TARGET, ADDRESS)        `INSTRUCTION(INST, TARGET, not_used, rom,     B,     `DIRECT,   ADDRESS, `NA)
-`define DEV_EQ_IMMED8(INST,TARGET, IMMED8)             `INSTRUCTION(INST, TARGET, not_used, instreg, B,     `REGISTER, `NA,     IMMED8) // src is the immed8 but target if ram is via MAR
-`define DEV_EQ_RAM_DIRECT(INST,TARGET, ADDRESS)        `INSTRUCTION(INST, TARGET, not_used, ram,     B,     `DIRECT,   ADDRESS, `NA)
-`define RAM_DIRECT_EQ_DEV(INST,ADDRESS, SRC)           `INSTRUCTION(INST, ram,    not_used, SRC,     B,     `DIRECT,   ADDRESS, `NA)
-
-`define CLEAR_CARRY(INST)   `DEV_EQ_IMMED8(INST, not_used, 0);
-`define SET_CARRY(INST)     `DEV_EQ_XI_ALU(INST, not_used, not_used, 255, B_PLUS_1)  // FIXME BROKEN COS B_PLUS_1 doesn't add
-
-// prep jump sourcing the PCHI from the immed8
-`define JMP_PREP_IMMED(INST, ADDRESS_HI)    `INSTRUCTION(INST, pchitmp, not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_HI) 
-// jump sourcing the PCLO from the immed8
-`define JMP_IMMED(INST, ADDRESS_LO)        `INSTRUCTION(INST, pc,      not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
-// conditional jump sourcing the PCLO from the immed8
-`define JMPZ_IMMED(INST, ADDRESS_LO)       `INSTRUCTION(INST, jmpz,    not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
-// conditional jump sourcing the PCLO from the immed8
-`define JMPC_IMMED(INST, ADDRESS_LO)       `INSTRUCTION(INST, jmpc,    not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
-`define JMPDO_IMMED(INST, ADDRESS_LO)      `INSTRUCTION(INST, jmpdo,   not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
-`define JMPDI_IMMED(INST, ADDRESS_LO)      `INSTRUCTION(INST, jmpdi,   not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
-
-`define JMP_IMMED16(INST, ADDRESS_LONG)       \
-  `JMP_PREP_IMMED(INST, ADDRESS_LONG >>  8) \
-  `JMP_IMMED(INST+1, ADDRESS_LONG & 8'hff)
-
-`define JMPC_IMMED16(INST, ADDRESS_LONG)       \
-  `JMP_PREP_IMMED(INST, ADDRESS_LONG >>  8) \
-  `JMPC_IMMED(INST+1, ADDRESS_LONG & 8'hff)
-
-`define JMPDO_IMMED16(INST, ADDRESS_LONG)       \
-  `JMP_PREP_IMMED(INST, ADDRESS_LONG >>  8) \
-  `JMPDO_IMMED(INST+1, ADDRESS_LONG & 8'hff)
-
-`define JMPDI_IMMED16(INST, ADDRESS_LONG)       \
-  `JMP_PREP_IMMED(INST, ADDRESS_LONG >>  8) \
-  `JMPDI_IMMED(INST+1, ADDRESS_LONG & 8'hff)
-
-/*
-    `PREPJMP_IMMED(INST, cast.hi8(ADDRESS_LONG)) \
-    `JMP_IMMED(INST+1, cast.lo8(ADDRESS_LONG))
-*/
+//
+//`define OUT_ADEV_SEL(DNAME) output _adev_``DNAME``
+//`define OUT_BDEV_SEL(DNAME) output _bdev_``DNAME``
+//`define OUT_TDEV_SEL(DNAME) output _``DNAME``_in
+//
+//`define DIRECT 1'b1
+//`define REGISTER 1'b0
+//`define NA_AMODE 1'b0
+//
+//// PSEUDO ASSEMBLER
+//`define ROM(A) { CPU.ctrl.rom_6.Mem[A], CPU.ctrl.rom_5.Mem[A], CPU.ctrl.rom_4.Mem[A], CPU.ctrl.rom_3.Mem[A], CPU.ctrl.rom_2.Mem[A], CPU.ctrl.rom_1.Mem[A] }
+//
+//// Instruction populates the ROM and adds a text version of the instruction to the CODE array
+//`define INSTRUCTION(LOCN, TARGET, SRCA, SRCB, ALUOP, AMODE, ADDRESS, IMMED) \
+//    `ROM(LOCN) = { `toALUOP(ALUOP), cast.to5(`toDEV(TARGET)), cast.to4(`toDEV(SRCA)), cast.to4(`toDEV(SRCB)), 5'bz, AMODE, cast.to16(ADDRESS), cast.to8(IMMED) }; \
+//    CODE[LOCN] = "TARGET=SRCA(ALUOP)SRCB  amode=AMODE immed8=IMMED addr=ADDRESS";
+//
+//`define NA 'z
+//
+//// WARNING : instreg in the X position is High Impedance as its only attached to the R bus
+//`define DEV_EQ_XI_ALU(INST, TARGET, SRCA, IMMED8, ALUOP) `INSTRUCTION(INST, TARGET, SRCA,    instreg,    ALUOP, `REGISTER, `NA,     IMMED8)
+//
+//`define DEV_EQ_XY_ALU(INST, TARGET, SRCA, SRCB, ALUOP) `INSTRUCTION(INST, TARGET, SRCA,     SRCB,    ALUOP, `REGISTER, `NA,     `NA)
+//`define DEV_EQ_ROM_DIRECT(INST,TARGET, ADDRESS)        `INSTRUCTION(INST, TARGET, not_used, rom,     B,     `DIRECT,   ADDRESS, `NA)
+//`define DEV_EQ_IMMED8(INST,TARGET, IMMED8)             `INSTRUCTION(INST, TARGET, not_used, instreg, B,     `REGISTER, `NA,     IMMED8) // src is the immed8 but target if ram is via MAR
+//`define DEV_EQ_RAM_DIRECT(INST,TARGET, ADDRESS)        `INSTRUCTION(INST, TARGET, not_used, ram,     B,     `DIRECT,   ADDRESS, `NA)
+//`define RAM_DIRECT_EQ_DEV(INST,ADDRESS, SRC)           `INSTRUCTION(INST, ram,    not_used, SRC,     B,     `DIRECT,   ADDRESS, `NA)
+//
+//`define CLEAR_CARRY(INST)   `DEV_EQ_IMMED8(INST, not_used, 0);
+//`define SET_CARRY(INST)     `DEV_EQ_XI_ALU(INST, not_used, not_used, 255, B_PLUS_1)  // FIXME BROKEN COS B_PLUS_1 doesn't add
+//
+//// prep jump sourcing the PCHI from the immed8
+//`define JMP_PREP_IMMED(INST, ADDRESS_HI)    `INSTRUCTION(INST, pchitmp, not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_HI) 
+//// jump sourcing the PCLO from the immed8
+//`define JMP_IMMED(INST, ADDRESS_LO)        `INSTRUCTION(INST, pc,      not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
+//// conditional jump sourcing the PCLO from the immed8
+//`define JMPZ_IMMED(INST, ADDRESS_LO)       `INSTRUCTION(INST, jmpz,    not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
+//// conditional jump sourcing the PCLO from the immed8
+//`define JMPC_IMMED(INST, ADDRESS_LO)       `INSTRUCTION(INST, jmpc,    not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
+//`define JMPDO_IMMED(INST, ADDRESS_LO)      `INSTRUCTION(INST, jmpdo,   not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
+//`define JMPDI_IMMED(INST, ADDRESS_LO)      `INSTRUCTION(INST, jmpdi,   not_used, instreg,    B,     `NA_AMODE, `NA, ADDRESS_LO) 
+//
+//`define JMP_IMMED16(INST, ADDRESS_LONG)       \
+//  `JMP_PREP_IMMED(INST, ADDRESS_LONG >>  8) \
+//  `JMP_IMMED(INST+1, ADDRESS_LONG & 8'hff)
+//
+//`define JMPC_IMMED16(INST, ADDRESS_LONG)       \
+//  `JMP_PREP_IMMED(INST, ADDRESS_LONG >>  8) \
+//  `JMPC_IMMED(INST+1, ADDRESS_LONG & 8'hff)
+//
+//`define JMPDO_IMMED16(INST, ADDRESS_LONG)       \
+//  `JMP_PREP_IMMED(INST, ADDRESS_LONG >>  8) \
+//  `JMPDO_IMMED(INST+1, ADDRESS_LONG & 8'hff)
+//
+//`define JMPDI_IMMED16(INST, ADDRESS_LONG)       \
+//  `JMP_PREP_IMMED(INST, ADDRESS_LONG >>  8) \
+//  `JMPDI_IMMED(INST+1, ADDRESS_LONG & 8'hff)
+//
 
 module wide_controller(
     input _mr,
