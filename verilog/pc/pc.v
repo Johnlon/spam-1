e18c3104a5f699f20ae43f480e6884e8e3f9efac
John Lonergan <john.lonergan@gmail.com>
2020-09-11 01:05:35 +0100

    simplified PC by using 74163 directly

--- verilog/pc/pc.v
+++ verilog/pc/pc.v
@@ -7,8 +7,8 @@
 // verilator lint_off STMTDLY
 
 `include "../lib/assertion.v"
-`include "../counterReg/counterReg.v"
 `include "../74377/hct74377.v"
+`include "../74163/hct74163.v"
 
 `timescale 1ns/1ns
 
@@ -24,12 +24,12 @@ module pc(
     output [7:0] PCHI
 );
 
-parameter LOG = 0;
+parameter LOG = 1;
 
 wire [7:0] PCHITMP;
 
 // low is loaded if separately loaded or both loaded
-wire #11 _pclo_load = _pclo_in & _pc_in;
+wire #11 _do_jump = _pclo_in & _pc_in;
 
 //wire #11 _gated_pchitmp_in = _pchitmp_in | clk;
     
@@ -41,39 +41,55 @@ hct74377 PCHiTmpReg(
 wire countEn;
 
 // 74163 counts when CEP/CET/PE are all high
-// _pclo_load is synchronous and must be held low DURING a +ve clk
+// _do_jump is synchronous and must be held low DURING a +ve clk
 
-assign countEn = _pclo_load; // _pclo_load is always involved in a jump so the inverse of this signal can enable count
+assign countEn = _do_jump; // inverse logic of this signal can enable count
 
 // see applications here https://www.ti.com/lit/ds/symlink/sn54ls161a-sp.pdf?ts=1599773093420&ref_url=https%253A%252F%252Fwww.google.com%252F
 // see ripple mode approach - CEP/CET can be tied high because _PE overrides those and so they can be left enabled.
 // TC/CET chains the count enable as per the data sheet for > 4 bits counting.
 
-counterReg LO
+// cascaded as per http://upgrade.kongju.ac.kr/data/ttl/74163.html
+// naming from https://www.ti.com/lit/ds/symlink/sn74f163a.pdf
+hct74163 PC_3_0
 (
   .CP(clk),
   ._MR(_MR),
   .CEP(1'b1),
   .CET(1'b1),
-  ._PE(_pclo_load),
-  .D(D),
-
-  .Q(PCLO)
+  ._PE(_do_jump),
+  .D(D[3:0])
 );
-
-counterReg HI
+hct74163 PC_7_4
 (
   .CP(clk),
   ._MR(_MR),
   .CEP(1'b1),
-  .CET(LO.TC),
+  .CET(PC_3_0.TC),
+  ._PE(_do_jump),
+  .D(D[7:4])
+);
+hct74163 PC_11_8
+(
+  .CP(clk),
+  ._MR(_MR),
+  .CEP(1'b1),
+  .CET(PC_7_4.TC),
   ._PE(_pc_in),
-  .D(PCHITMP),
-
-  .Q(PCHI),
-  .TC() // ignored out
+  .D(PCHITMP[3:0])
+);
+hct74163 PC_16_12
+(
+  .CP(clk),
+  ._MR(_MR),
+  .CEP(1'b1),
+  .CET(PC_11_8.TC),
+  ._PE(_pc_in),
+  .D(PCHITMP[7:4])
 );
 
+assign PCLO = {PC_7_4.Q, PC_3_0.Q};
+assign PCHI = {PC_16_12.Q, PC_11_8.Q};
 
 if (LOG) always @(posedge clk)
 begin
@@ -93,8 +109,8 @@ if (LOG) always @(*) begin
       PCHI, PCLO, PCHITMP,
       "clk=%1b ",  clk, 
       "_MR=%1b ",  _MR, 
-      " countEn=%1b _pclo_in=%1b _pc_in=%1b _pclo_load=%1b _pchitmp_in=%1b     Din=%8b ",
-      countEn, _pclo_in, _pc_in, _pclo_load, _pchitmp_in, D 
+      " countEn=%1b _pclo_in=%1b _pc_in=%1b _do_jump=%1b _pchitmp_in=%1b     Din=%8b ",
+      countEn, _pclo_in, _pc_in, _do_jump, _pchitmp_in, D 
       );
 end
 
