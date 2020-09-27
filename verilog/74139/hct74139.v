74107ef701549960d6cd9cb770053345fb7bb9b0
John Lonergan <john.lonergan@gmail.com>
2020-09-28 00:54:22 +0100

    use non blocking assign to set delays as they make glitches visible

--- verilog/74139/hct74139.v
+++ verilog/74139/hct74139.v
@@ -3,29 +3,24 @@
 // DUAL 2 to 4 line decoder
 // https://books.google.co.uk/books?id=pkW5DwAAQBAJ&pg=PA362&lpg=PA362&dq=verilog+%2274139%22&source=bl&ots=p5Lb0vS6Gc&sig=ACfU3U39tOIUbv0jCeofuICsNqxWo7PJJA&hl=en&sa=X&ved=2ahUKEwjVp6ecno3oAhXEoFwKHdR-BX0Q6AEwAXoECDIQAQ#v=onepage&q=verilog%20%2274139%22&f=false
 
-// Following naming used by https://assets.nexperia.com/documents/data-sheet/74HC_HCT139.pdf
+// see also discussion on delays that tranmit glitches ...
+// https://stackoverflow.com/questions/64083950/why-are-icarus-verilog-specify-times-not-respected/64085508?noredirect=1#comment113337822_64085508
+
+// names and HCT timings from https://assets.nexperia.com/documents/data-sheet/74HC_HCT139.pdf
 `timescale 1ns/1ns
 module hct74139(
 input _Ea, 
 input _Eb, 
 input [1:0] Aa,
 input [1:0] Ab,
-output [3:0] _Ya,
-output [3:0] _Yb
+output logic [3:0] _Ya,
+output logic [3:0] _Yb
 ); 
 
     parameter LOG = 0;
 
-    // HCT timings from https://assets.nexperia.com/documents/data-sheet/74HC_HCT139.pdf
     parameter AY_PD = 13;
     parameter EY_PD = 13;
-
-    specify
-      (_Ea *> _Ya) = (EY_PD);
-      (Aa *> _Ya) = (AY_PD);
-      (_Eb *> _Yb) = (EY_PD);
-      (Ab *> _Yb) = (AY_PD);
-    endspecify
 
     function [3:0] f74139(input [1:0] A, input _E); 
 
@@ -48,8 +43,26 @@ output [3:0] _Yb
 
     endfunction
 
-    assign _Ya = f74139(Aa,_Ea);
-    assign _Yb = f74139(Ab,_Eb);
+
+    logic _Ea_d;
+    logic _Eb_d;
+    logic [1:0] Aa_d;
+    logic [1:0] Ab_d;
+
+    always @*
+         Aa_d <= #(AY_PD) Aa;
+    always @*
+         Ab_d <= #(AY_PD) Ab;
+    always @*
+         _Ea_d <= #(EY_PD) _Ea;
+    always @*
+         _Eb_d <= #(EY_PD) _Eb;
+
+    always @*
+         _Ya <= f74139(Aa_d,_Ea_d);
+
+    always @*
+        _Yb <= f74139(Ab_d,_Eb_d);
 
    if (LOG) always @*
         $display("%9t ", $time, " DEMUX   _Ea=%1b", _Ea, " Aa=%2b", Aa, " _Ya=%4b", _Ya, "  /  _Eb=%1b", _Eb, " Ab=%2b", Ab, " _Yb=%4b", _Yb);
