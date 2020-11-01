4dc95d8848d2468673e5bdc67c52c9064470d1c1
John Lonergan <john.lonergan@gmail.com>
2020-11-01 02:26:33 +0000

    logging

--- verilog/lib/assertion.v
+++ verilog/lib/assertion.v
@@ -76,23 +76,23 @@ end
 
 `ifndef verilator
 // never returns
-`define DOUBLE_CHECK(EXPR,TIMEOUT, st) begin \
+`define DOUBLE_CHECK(EXPR,ORIG_TIMOUT, FACTOR, st) begin \
             fork \
               begin \
-                  #(TIMEOUT) \
-                  $display("%9t", $time, " !!! TIMED OUT - NEVER MET AFTER HARD TIME OUT %1d FOR EXPR (got %b)", ($time-st), EXPR , "   LINE %1d",  `__LINE__); \                
+                  #(ORIG_TIMOUT*FACTOR) \
+                  $display("%9t", $time, " !!! TIMED OUT - NEVER MET AFTER HARD TIME OUT %1d FOR EXPR (got %b)", ($time-st), EXPR , "   LINE %1d",  `__LINE__); \
                   $finish_and_return(1); \
               end \
               begin \
                 wait(EXPR); \
                 if (EXPR) begin \
-                  $display("%9t", $time, " !!! TIMED OUT - BUT CONDITION MET AFTER %1d FOR EXPR (got %b)", ($time-st), EXPR , "   LINE %1d",  `__LINE__); \
+                  $display("%9t", $time, " !!! TIMED OUT EXPECTING ORIG_TIMOUT (%1d) - BUT CONDITION MET AFTER %1d FOR EXPR (got %b)", (ORIG_TIMOUT), ($time-st), EXPR , "   LINE %1d",  `__LINE__); \
                 end \
                 $finish_and_return(12); \
               end \
             join_none \
-            #(TIMEOUT) \
-            $display("%9t", $time, " !!! TIMED OUT - NEVER MET AFTER HARD TIME OUT %1d FOR EXPR (got %b)", ($time-st), EXPR , "   LINE %1d",  `__LINE__); \                
+            #(ORIG_TIMOUT*FACTOR) \
+            $display("%9t", $time, " !!! TIMED OUT - NEVER MET AFTER HARD TIME OUT %1d FOR EXPR (got %b)", ($time-st), EXPR , "   LINE %1d",  `__LINE__); \
             $finish_and_return(1); \
           end
 
@@ -132,12 +132,12 @@ end
               else \
               if (ok && (took == TIMEOUT)) \
               begin \
-                $display("%9t", $time, " DELAY OK- TOOK %1d  - EXPR ", ($time-st-1)); \
+                $display("%9t", $time, " DELAY OK- TOOK %1d  - EXPR ", ($time-st)); \
               end \
               else \
               if (timed_out == 1) \
               begin \
-                `DOUBLE_CHECK(EXPR, 10*TIMEOUT, st) /* HARD LIMIT IIS A MULTIPLE */  \
+                `DOUBLE_CHECK(EXPR, TIMEOUT, 10, st) /* HARD LIMIT IIS A MULTIPLE */  \
                 $finish_and_return(1); \
               end \
               else \
