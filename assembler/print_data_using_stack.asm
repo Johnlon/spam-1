008f83c1157d139c33e67ad84ca49cb2bc666bc5
John Lonergan <john.lonergan@gmail.com>
2020-11-23 03:06:45 +0000

    print_data_using_stack.asm works

--- assembler/print_data_using_stack.asm
+++ assembler/print_data_using_stack.asm
@@ -1,37 +1,35 @@
 ; run and grep "TRANS" to see uart transmit message
 
-STACKTOP: EQU $ffff     ; location of top of stack
-STACKPTR: EQU $feff     ; location of low byte of stack ptr addressa, top byte is a const, therefore 255 byte stack
-STACKPTR_ADDR_HI: EQU <:STACKTOP   ;fixed
-[:STACKPTR] = >:STACKTOP        ; ram location
-
-CURCHAR_LO: EQU 0
-CURCHAR_HI: EQU (:CURCHAR_LO+1)
+STACKTOP: EQU $ffff                 ; location of top of stack
+STACKPTR_ADDR_HI: EQU <:STACKTOP    ;const value pointing at pag stack is on
+STACKPTR_ADDR_LO: EQU $feff         ;location of low byte of stack ptr addressa, top byte is a const, therefore 255 byte stack
 
 ; LOAD TO RAM AT THE CURRENT MEMORY[0] ONWARDSa AND SET THE LABEL TO THE FIRST BYTE
 STRING_DATA:     STR     "Hello World\0"
 
-            PCHITMP = 0
+CURCHAR_LO:     BYTES [ > :STRING_DATA ]   ; where to store a pointer into the string initialise to the start of the string
+CURCHAR_HI:     BYTES [ < :STRING_DATA ]
 
-            [:CURCHAR_HI] = < :STRING_DATA
-            [:CURCHAR_LO] = > :STRING_DATA
+            [:STACKPTR_ADDR_LO] = >:STACKTOP        ; ram location
+
+            PCHITMP = 0
 
 loop:
             ; read next char into REGA
             MARHI=[:CURCHAR_HI]
-            MARLO=[:CURCHAR_LO]
+            MARLO=[:CURCHAR_LO]  ; 17
             REGA=RAM _S
 
-            ; goto end if printed last char
+            ; goto end if just read a 0 ie last char
             PC = >:end _Z
 
             ; ===================================BEGIN CALL
             ; ===================================SETUP ARGS
             ; recover stack pointer into MAR
             MARHI=:STACKPTR_ADDR_HI
-            MARLO=[:STACKPTR]
+            MARLO=[:STACKPTR_ADDR_LO]
 
-            RAM=>:loop  ; put return address on stack
+            RAM=>:next_char  ; put return address on stack
 
             ; move stack ptr
             MARLO=MARLO - 1
@@ -40,7 +38,7 @@ loop:
             RAM=REGA
 
             ; move and save stack ptr
-            [:STACKPTR] = MARLO-1
+            [:STACKPTR_ADDR_LO] = MARLO-1
             ; end jump
 
             ; ===================================DO JUMP
@@ -48,18 +46,28 @@ loop:
             PC      = >:send_uart
             ; ===================================END CALL
 
+next_char:
+            MARHI=[:CURCHAR_HI]
+            MARLO=NU B_PLUS_1 [:CURCHAR_LO] _S
+            MARHI=MARHI+1 _C
+
+            [:CURCHAR_HI]=MARHI
+            [:CURCHAR_LO]=MARLO
+
+            PC      = >:loop
 
 send_uart:
             ; ==================================RECOVER ARGS
             ; recover stack ptr
             MARHI=:STACKPTR_ADDR_HI
-            MARLO=[:STACKPTR]
+            MARLO=[:STACKPTR_ADDR_LO]
 
-            ; move sp
-            [:STACKPTR]=MARLO+1
+            ; move and save sp
+            MARLO=MARLO+1
+            [:STACKPTR_ADDR_LO]=MARLO
 
             ; recover 1st arg
-            REGA=RAM
+            REGA=RAM ; READED UNINIT LOCATIONi fffd !
             ; ===================================END RECOVER ARGS
 
 wait_for_tx_ready:
@@ -67,15 +75,18 @@ wait_for_tx_ready:
             PC      = >:wait_for_tx_ready    ;loop wait
 
 transmit:
-            ; write te byute
-            UART=RAM
+            ;MARHI=:STACKPTR_ADDR_HI
+            ;MARLO=[:STACKPTR]
+
+            ; write te uart
+            UART=REGA
 
 return:
             ; ===================================START DO RETURN
-            ; recover move and save sp
+            ; recover return address and save sp
             MARHI=:STACKPTR_ADDR_HI
-            MARLO=[:STACKPTR]
-            [:STACKPTR]=MARLO+1
+            MARLO=NU B_PLUS_1 [:STACKPTR_ADDR_LO]
+            [:STACKPTR_ADDR_LO]=MARLO
 
             PC = RAM
             ; ===================================END DO RETURN
