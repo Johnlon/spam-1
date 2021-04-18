17edd030f0240ba6d4d979570848241766d4bba8
John Lonergan <john.lonergan@gmail.com>
2021-04-18 17:09:00 +0100

    use A/B not L/R

--- verilog/registerFile/registerFile.v
+++ verilog/registerFile/registerFile.v
@@ -13,95 +13,95 @@ module registerFile #(parameter LOG=0) (
                     input [1:0] wr_addr,
                     input [7:0] wr_data,
 
-                    input _rdL_en,
-                    input [1:0] rdL_addr,
-                    output [7:0] rdL_data,
+                    input _rdA_en,
+                    input [1:0] rdA_addr,
+                    output [7:0] rdA_data,
 
-                    input _rdR_en,
-                    input [1:0] rdR_addr,
-                    output [7:0] rdR_data
+                    input _rdB_en,
+                    input [1:0] rdB_addr,
+                    output [7:0] rdB_data
 	    );
 
     
     wire [3:0] wr_data_hi, wr_data_lo;
 // verilator lint_off UNOPTFLAT
-    wire [3:0] rdL_data_hi, rdL_data_lo, rdR_data_hi, rdR_data_lo;
+    wire [3:0] rdA_data_hi, rdA_data_lo, rdB_data_hi, rdB_data_lo;
 // verilator lint_on UNOPTFLAT
 
     assign {wr_data_hi, wr_data_lo} = wr_data;
-    assign rdL_data                 = {rdL_data_hi, rdL_data_lo};
-    assign rdR_data                 = {rdR_data_hi, rdR_data_lo};
+    assign rdA_data                 = {rdA_data_hi, rdA_data_lo};
+    assign rdB_data                 = {rdB_data_hi, rdB_data_lo};
 
-    hct74670 bankL_lo(
+    hct74670 bankA_lo(
         _wr_en,
         wr_addr,
         wr_data_lo,
-        _rdL_en,
-        rdL_addr,
-        rdL_data_lo
+        _rdA_en,
+        rdA_addr,
+        rdA_data_lo
     );
-    hct74670 bankL_hi(
+    hct74670 bankA_hi(
         _wr_en,
         wr_addr,
         wr_data_hi,
-        _rdL_en,
-        rdL_addr,
-        rdL_data_hi
+        _rdA_en,
+        rdA_addr,
+        rdA_data_hi
     );
     
-    hct74670 bankR_lo(
+    hct74670 bankB_lo(
         _wr_en,
         wr_addr,
         wr_data_lo,
-        _rdR_en,
-        rdR_addr,
-        rdR_data_lo
+        _rdB_en,
+        rdB_addr,
+        rdB_data_lo
     );
-    hct74670 bankR_hi(
+    hct74670 bankB_hi(
         _wr_en,
         wr_addr,
         wr_data_hi,
-        _rdR_en,
-        rdR_addr,
-        rdR_data_hi
+        _rdB_en,
+        rdB_addr,
+        rdB_data_hi
     );
     
     if (LOG) always @(negedge _wr_en) begin 
         $display("%9t REGFILE : BEGIN WRITE _wr_en=%1b,  write[%d]=%-3d     _rdX_en=%1b, X[%d]=>%-3d    _rdY_en=%1b, Y[%d]=>%-3d" , $time, 
                     _wr_en, wr_addr, wr_data, 
-                    _rdL_en, rdL_addr, rdL_data, 
-                    _rdL_en, rdR_addr, rdR_data);
+                    _rdA_en, rdA_addr, rdA_data, 
+                    _rdA_en, rdB_addr, rdB_data);
     end
     if (LOG) always @(posedge _wr_en) begin 
         $display("%9t REGFILE : END WRITE _wr_en=%1b,  write[%d]=%-3d     _rdX_en=%1b, X[%d]=>%-3d    _rdY_en=%1b, Y[%d]=>%-3d" , $time, 
                     _wr_en, wr_addr, wr_data, 
-                    _rdL_en, rdL_addr, rdL_data, 
-                    _rdL_en, rdR_addr, rdR_data);
+                    _rdA_en, rdA_addr, rdA_data, 
+                    _rdA_en, rdB_addr, rdB_data);
     end
 
     function [7:0] get([1:0] r);
-        get = {bankL_hi.registers[r], bankL_lo.registers[r]};
+        get = {bankA_hi.registers[r], bankA_lo.registers[r]};
     endfunction
 
     // only need to bind to L or R as they have the same value
     if (LOG) always @(   
                     _wr_en//, wr_addr, wr_data, 
-                    //_rdL_en, rdL_addr, rdL_data, 
-                    //_rdL_en, rdR_addr, rdR_data
+                    //_rdA_en, rdA_addr, rdA_data, 
+                    //_rdA_en, rdB_addr, rdB_data
     ) begin
         $display("%9t ", $time, "REGFILE : _wr_en=%1b", //,  write[%d]=%-3d     _rdX_en=%1b, X[%d]=>%-3d    _rdY_en=%1b, Y[%d]=>%-3d" , 
                     _wr_en//, wr_addr, wr_data, 
-                    //_rdL_en, rdL_addr, rdL_data, 
-                    //_rdL_en, rdR_addr, rdR_data
+                    //_rdA_en, rdA_addr, rdA_data, 
+                    //_rdA_en, rdB_addr, rdB_data
                     );
     end
 
 `ifndef verilator
     if (LOG) always @(   
-                bankR_hi.registers[0] or bankR_lo.registers[0] or
-                bankR_hi.registers[1] or bankR_lo.registers[1] or
-                bankR_hi.registers[2] or bankR_lo.registers[2] or
-                bankR_hi.registers[3] or bankR_lo.registers[3]
+                bankB_hi.registers[0] or bankB_lo.registers[0] or
+                bankB_hi.registers[1] or bankB_lo.registers[1] or
+                bankB_hi.registers[2] or bankB_lo.registers[2] or
+                bankB_hi.registers[3] or bankB_lo.registers[3]
     ) begin
         $display("%9t ", $time, "REGFILE : DATA UPDATE A=%-3d(%-2x) B=%-3d(%-2x) C=%-3d(%-2x) D=%-3d(%-2x)", 
                 get(0), get(0), 
