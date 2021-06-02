5f3397af6000d246407e3d93a3e9693b4b265e0f
John Lonergan <john.lonergan@gmail.com>
2021-06-02 01:20:35 +0100

    added divide program

--- jvmtools/programs/scc_chip8.sh
+++ jvmtools/programs/scc_chip8.sh
@@ -5,8 +5,13 @@ if [ -z $scc_file ]; then
     echo "$0: missing arg 'scc_file'"
     exit 1
 fi
+chip8_prog=$2
+if [ -z $chip8_prog ]; then 
+    echo "$0: missing arg 'chip8_prog'"
+    exit 1
+fi
 
-java -classpath ../build/libs/jvmtools.jar  scc/SpamCC $scc_file
+java -DCHIP8_FILENAME=$chip8_prog -classpath ../build/libs/jvmtools.jar  scc/SpamCC $scc_file
 if [ $? -ne 0 ]; then
     echo error
     exit 1
